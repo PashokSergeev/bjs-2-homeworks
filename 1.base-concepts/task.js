d04cac88a9786434f635dfa48871d3878d6be7ff
Pavel <pavel_sergeev_2003@bk.ru>
2025-02-26 13:52:54 +0300

    Решение задачи 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,22 +1,26 @@
 "use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
+    let arr = [];
 
-  let d = b**2 - 4 * a * c;
-  if (d > 0){
-    arr.push((-b + Math.sqrt(d)) / (2 * a) );
-    arr.push((-b - Math.sqrt(d)) / (2 * a) );
-  }else if (d === 0){
-    arr.push(-b / (2 * a) );
-  }
+    let d = b ** 2 - 4 * a * c;
+    if (d > 0) {
+        arr.push((-b + Math.sqrt(d)) / (2 * a));
+        arr.push((-b - Math.sqrt(d)) / (2 * a));
+    } else if (d === 0) {
+        arr.push(-b / (2 * a));
+    }
 
-  return arr;
+    return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  percent = percent / 100 / 12;
-  let credit = amount - contribution;
-  let payment = credit * (percent + (percent / (((1 + percent)**countMonths) - 1)));
-
-  return payment.toFixed(2);
+    percent = percent / 100 / 12;
+    let credit = amount - contribution;
+    let payment = credit * (percent + (percent / (((1 + percent) ** countMonths) - 1)));
+    if (credit === 0) {
+        return 0;
+    } else {
+        return parseFloat((contribution + payment * countMonths).toFixed(2));
+    }
 }
