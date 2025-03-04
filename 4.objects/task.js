6d1465b52391ee835c6782464f1919c3f2b9a9b8
Pavel <pavel_sergeev_2003@bk.ru>
2025-03-04 13:50:30 +0300

    Решение задачи c объектами 4.1-3

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,28 @@
 function Student(name, gender, age) {
-  
+    let marks = [];
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    return {name, gender, age, marks};
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    this.marks = [...marks];
 }
 
 Student.prototype.getAverage = function () {
-  
+
 }
 
 Student.prototype.exclude = function (reason) {
-  
+
 }
+
+
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1);
