b7da0ac230f74f5a47fb7c267791cad61db970b9
Pavel <pavel_sergeev_2003@bk.ru>
2025-03-05 14:14:43 +0300

    Решение задачи c классами

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -4,20 +4,22 @@ function Student(name, gender, age) {
     this.gender = gender;
     this.age = age;
 }
-let student1 = new Student("Tony", "male", 37);
-let student2 = new Student("Buzz", "female", 35);
 
 Student.prototype.setSubject = function (subjectName) {
     this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-    if(this.excluded) return;
+    if(this.excluded) {
+        return;
+    }
     this.marks.push(...marks);
 }
 
 Student.prototype.getAverage = function () {
-    if (!this.marks || this.marks.length === 0) return 0;
+    if (!this.marks || this.marks.length === 0) {
+        return 0;
+    }
     const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
     return sum / this.marks.length;
 }
